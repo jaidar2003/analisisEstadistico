77395d513f133307ca9874392f8e939a25613ef9
juanma <j.aidar@alumno.um.edu.ar>
2024-05-23 11:04:32 -0300

    Enhanced console output formatting for analysis summary and added new parameters to statistical analysis

--- main.py
+++ main.py
@@ -17,15 +17,57 @@ def realizar_analisis(archivo_excel):
     analisis.cargar_datos()
     mostrar_resultados(analisis)
 
+
 def mostrar_resultados(analisis):
-    print("\nDatos:")
-    analisis.mostrar_datos()
 
-    print("\nResumen estadístico:")
+    print("\n--- Resumen Estadístico ---")
+    print("")
+
+    print("Resumen estadístico general:")
     analisis.resumen_estadistico()
+    print("")
 
-    print("\nEstadísticas de la columna 'columna1':")
-    analisis.estadisticas_columna("columna1")
+    print("Estadísticas por asignatura:")
+    print("")
+
+    print("   - Math Score:")
+    analisis.estadisticas_columna("math_score")
+    print("")
+
+    print("   - Reading Score:")
+    analisis.estadisticas_columna("reading_score")
+    print("")
+
+    print("   - Writing Score:")
+    analisis.estadisticas_columna("writing_score")
+    print("")
+
+    print("Estadísticas comparativas según el curso de preparación:")
+    print("")
+
+    estudiantes_estudiaron = analisis.df[analisis.df["test_preparation_course"] == "completed"]
+    print("   - Estudiantes que estudiaron:")
+    analisis.resumen_estadistico_para_grupo(estudiantes_estudiaron)
+    print("")
+
+    estudiantes_no_estudiaron = analisis.df[analisis.df["test_preparation_course"] == "none"]
+    print("   - Estudiantes que no estudiaron:")
+    analisis.resumen_estadistico_para_grupo(estudiantes_no_estudiaron)
+    print("")
+
+    print("Estadísticas comparativas según el tipo de almuerzo:")
+    print("")
+
+    estudiantes_comieron = analisis.df[analisis.df["lunch"] == "standard"]
+    print("   - Estudiantes que comieron almuerzo estándar:")
+    analisis.resumen_estadistico_para_grupo(estudiantes_comieron)
+    print("")
+
+    estudiantes_no_comieron = analisis.df[analisis.df["lunch"] == "free/reduced"]
+    print("   - Estudiantes que no comieron almuerzo estándar:")
+    analisis.resumen_estadistico_para_grupo(estudiantes_no_comieron)
+    print("")
+
 
 if __name__ == "__main__":
     main()
